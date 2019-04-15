ddb89bb6c1e6e501dea36abf8d8dbd33064d64c4
Du <49415203+westlife29@users.noreply.github.com>
2019-04-15 12:33:42 -0400

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -12,7 +12,12 @@ class QuizQuestion extends Component{
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((answer_option, index) =>
+            {
+              return <QuizQuestionButton key ={index} button_text ={answer_option} />
+            })
+            }
+            
           </ul>
         </section>
       </main>
@@ -22,4 +27,3 @@ class QuizQuestion extends Component{
 
 
 export default QuizQuestion;
-
