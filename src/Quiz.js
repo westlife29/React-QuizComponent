f9172d04799668abe81634a6ab106c89b0493723
Du <49415203+westlife29@users.noreply.github.com>
2019-04-15 12:10:37 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component
         this.state = {quiz_position: 1}
     }
     render(){
-        const isQuizEnd = true
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return(
             <div>
                 {isQuizEnd ? (<QuizEnd />) :
