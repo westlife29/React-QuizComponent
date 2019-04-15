eb3abf7f2319c15a68aa28b439f69b14d2f0e576
Du <49415203+westlife29@users.noreply.github.com>
2019-04-15 11:26:30 -0400

    Update QuizEnd.js

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -6,10 +6,10 @@ class QuizEnd extends Component
     {
         return (
             <div>
-                <p> Thanks for playing!</p>
+                <p>Thanks for playing!</p>
                 <a href=''>Reset Quiz</a>
             </div>
-        );
+        )
     }
 }
 
