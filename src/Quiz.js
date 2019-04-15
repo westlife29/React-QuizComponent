66d8239b98c032fe655ba495561ac83ed123ed55
Du <49415203+westlife29@users.noreply.github.com>
2019-04-15 13:04:19 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,14 +11,23 @@ class Quiz extends Component
         super(props)
         this.state = {quiz_position: 1}
     }
+    showNextQuestion()
+    {
+        this.setState((state) =>{
+            return {quiz_position: state.quiz_position + 1}
+        })
+    }
     render(){
-        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)//this will return boolen to check the logic in return
         return(
             <div>
-                {isQuizEnd ? (<QuizEnd />) :
-                 (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} />)}
+                {/* This condition statemtent {isQuizEnd ? true : false}*/}
+                {isQuizEnd ?                
+                <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} showNextQuestionHandler ={this.showNextQuestion.bind(this)}/>
+                }
             </div>
-        )
+        );
     }
 }
 
