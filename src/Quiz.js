74e7fbe4dec2a1a11bb5f84c9b4935641c180b23
Du <49415203+westlife29@users.noreply.github.com>
2019-04-15 11:51:04 -0400

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,7 +16,8 @@ class Quiz extends Component
         return(
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>}
+                 <QuizQuestion quiz_question={quizData.quiz_questions
+                 [this.state.quiz_position -1]} />}
             </div>
         )
     }
